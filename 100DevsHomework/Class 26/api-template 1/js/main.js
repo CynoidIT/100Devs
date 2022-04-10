f35a60fb1a872aefdc99980e8f9a81fe4359996f
cynoidit <cynoidit@gmail.com>
2022-04-10 15:16:49 -0700

    updates

--- 100DevsHomework/Class 26/api-template 1/js/main.js
+++ 100DevsHomework/Class 26/api-template 1/js/main.js
@@ -2,13 +2,16 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  // let choice = ''
+  let choice = document.getElementById('participants').value
+
+  const url = 'https://www.boredapi.com/api/activity?participants='+choice
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        document.getElementById('activity').innerText = data.activity
       })
       .catch(err => {
           console.log(`error ${err}`)

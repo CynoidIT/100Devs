845ed2dfdc410eb0e46019d3de2cbcbd19aa0521
cynoidit <cynoidit@gmail.com>
2022-04-10 20:11:19 -0700

    updates

--- 100DevsHomework/Class 26/api-hungry/js/main.js
+++ 100DevsHomework/Class 26/api-hungry/js/main.js
@@ -3,15 +3,16 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
   // let choice = ''
-  let choice = document.getElementById('participants').value
-
-  const url = 'https://www.boredapi.com/api/activity?participants='+choice
-
+  let choice = document.getElementById('mainIngredient').value
+  console.log(choice)
+  let url = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='+choice
+  console.log(url)
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.getElementById('activity').innerText = data.activity
+        document.getElementById('meal').innerText = data.meals[0].strMeal
+        document.querySelector('img').src = data.meals[0].strMealThumb
       })
       .catch(err => {
           console.log(`error ${err}`)
--- 100DevsHomework/Class 26/api-template 3/js/main.js
+++ 100DevsHomework/Class 26/api-template 3/js/main.js
@@ -1,19 +1,18 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
-
-function getFetch(){
-  // let choice = ''
-  let choice = document.getElementById('participants').value
-
-  const url = 'https://www.boredapi.com/api/activity?participants='+choice
-
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.getElementById('activity').innerText = data.activity
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+fetch('https://api.ipify.org/?format=json')
+    .then(response => response.json()
+    )
+    .then(data => fetch('https://api.techniknews.net/ipgeo/' + data.ip)
+    )
+    .then(response => response.json()
+    )
+    //.then(data => document.getElementById('location').innerText = data.city)
+    .then(data => fetch(`https://api.open-meteo.com/v1/forecast?latitude=${data.lat}&longitude=${data.lon}&hourly=temperature_2m&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch`)
+    )
+    
+    .then(response => response.json()
+    )
+    .then(data => {
+      console.log(data)
+      document.getElementById('temperature').innerText = data.current_weather.temperature
+    })
+ 

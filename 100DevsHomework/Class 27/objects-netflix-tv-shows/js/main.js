46ea3a690acd923f760b0aaf2927145e12e4d788
cynoidit <cynoidit@gmail.com>
2022-04-12 16:34:19 -0700

    updates

---
+++ 100DevsHomework/Class 27/objects-netflix-tv-shows/js/main.js
@@ -0,0 +1,19 @@
+//Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+
+class NetflixTVShow {
+    constructor(iq,about,length,remake) {
+        this.stupid = iq
+        this.about = about
+        this.length = length
+        this.remake = remake
+    }
+    advertise(){
+        alert('Annoying Advert')
+    }
+    something(){
+        console.log('boring')
+    }
+    another(){
+        console.log('hack')
+    }
+}

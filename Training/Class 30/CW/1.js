6fb05034f1d6f6435d331051b6258712188105a2
cynoidit <cynoidit@gmail.com>
2022-05-02 22:33:33 -0700

    updates

---
+++ Training/Class 30/CW/1.js
@@ -0,0 +1,63 @@
+//return price without vat
+function excludingVatPrice(price){
+    if (price === 0){
+      return 0
+      }else if (!price){
+        console.log(price)
+        return -1
+      }else {
+        let answer = (price /(1+.15))
+        return(parseFloat(answer.toFixed(2)))
+      } 
+  }
+
+
+// create a function which returns an RNA sequence from the given DNA sequence
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g,'U')
+}
+
+//Count the Monkeys!
+function monkeyCount(n) {
+    let arr = []
+    for (i=1;i<=n;i++){
+      arr.push(i)
+    }
+    return arr
+  }
+
+
+//Sentence smash  
+  function smash (words) {
+    return words.join(' ')
+ };
+
+
+// Write here your multiply-function
+
+function multiply(n1,n2){
+    return n1*n2
+  }
+
+
+//If you can't sleep, just count sheep!!
+var countSheep = function (num){
+    let answer = ''
+    for (i=1;i<=num;i++){
+      answer += `${i} sheep...`
+    }
+    return answer
+  }
+
+
+//Transportation on vacation 
+function rentalCarCost(d) {
+    let subT = d*40
+    if (d>=7){
+      return subT - 50
+    }else if (d>=3){
+      return subT - 20
+    }else {
+      return subT
+    }
+  }

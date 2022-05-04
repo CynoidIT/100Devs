c63b33b355d88f6455d90417a7acded4887b21b7
cynoidit <cynoidit@gmail.com>
2022-05-03 17:56:18 -0700

    updates

--- Training/Class 31/complete/js/main.js
+++ Training/Class 31/complete/js/main.js
@@ -1,47 +1,122 @@
 class Animal{
-    constructor(name){
+    constructor(name, sound, breed){
         this._name = name
+        this._sound = sound
+        this._breed = breed
     }
     get name(){
         return this._name
     }
-    speak(){
-        console.log(`${this._name} makes a sound`)
+    get sound(){
+        return this._sound
     }
+    get breed() {
+        return this._breed
+    }
+    speak(){
+        console.log(`${this._name} goes ${this._sound}`)
+    }
+
+
 }
+
 class Dog extends Animal{
-    constructor(name,breed){
+    constructor(name, sound, breed){
         super(name)
-        this._breed = breed
+        super(sound)
+        super(breed)
     }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
 }
+
 class Cat extends Animal{
-    constructor(name,breed){
+    constructor(name, sound, breed){
         super(name)
-        this._breed = breed
+        super(sound)
+        super(breed) 
     }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
 
-let farm = [simba,machi,salem]
 
-for( a of farm ){
-    a.speak()
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} barks`)
+//     }    
+// }
+// class Cat extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} meows`)
+//     }    
+// }
+
+// class Cow extends Animal{
+//     constructor(name, breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     speak(){
+//         console.log(`${this.name} moos`)
+//     }
+// }
+
+// let bessy = new Cow('Bessy', 'Herereford')
+
+// let simba = new Dog('Simba','Shepard')
+// let machi = new Dog('The Machine','Pitbull')
+// let salem = new Cat('Salem', 'American Shorthair')
+
+// let farm = [simba,machi,salem, bessy]
+
+// for( a of farm ){
+//     a.speak()
+// }
+

192811b39b29f914c4710c0755fceb056235378a
cynoidit <cynoidit@gmail.com>
2022-05-08 21:08:44 -0700

    updates to CW

--- Training/CW/1.js
+++ Training/CW/1.js
@@ -80,3 +80,18 @@ const areaOrPerimeter = function(l , w) {
     return 2*w + 2*l
     }
 };
+
+
+//Quarter of the year
+
+const quarterOf = (month) => {
+  if (month < 4)
+     return 1;
+  else if (month < 7)
+      return 2;
+  else if (month < 10)
+      return 3;
+  else 
+      return 4;
+  
+}

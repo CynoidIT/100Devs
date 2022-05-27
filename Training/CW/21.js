10062b28d6416e75e0ffb11e5bcf3d9b81d9ed34
cynoidit <cynoidit@gmail.com>
2022-05-27 14:32:28 -0700

    added cw 21

---
+++ Training/CW/21.js
@@ -0,0 +1,8 @@
+//Extract the domain name from a URL
+
+function domainName(url){
+    url = url.replace("https://", '');
+    url = url.replace("http://", '');
+    url = url.replace("www.", '');
+    return url.split('.')[0];
+  };

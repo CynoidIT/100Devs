833b84535f65ec32a81e1aded724a1ed287ac3af
cynoidit <cynoidit@gmail.com>
2022-05-28 09:36:59 -0700

    added cw 22

---
+++ Training/CW/22.js
@@ -0,0 +1,10 @@
+// Sum Arrays
+
+function sum (numbers) {
+    console.log (numbers)
+    let answer = 0
+    for(i=0;i<numbers.length;i++){
+      answer += numbers[i]
+    }
+    return answer    
+  };
